698893b510e98597dd4a13f1498c106ad0ff2780
AlejandroGarciaPerez1999 <alegarper10@alu.edu.gva.es>
2025-10-22 19:32:26 +0200

    Test factorial completado

--- TestPruebasUnitariasApp/CalcularFactorial.cs
+++ TestPruebasUnitariasApp/CalcularFactorial.cs
@@ -1,11 +1,29 @@
-﻿namespace TestPruebasUnitariasApp
+﻿using Microsoft.VisualStudio.TestPlatform.TestHost;
+
+namespace TestPruebasUnitariasApp
 {
     [TestClass]
-    public sealed class Test1
+    public sealed class CalcularFactorial
     {
         [TestMethod]
-        public void TestMethod1()
+        public void CalcularFactorial_DevolverMenosUno_SiNegativo()
         {
+            long res = Program.CalcularFactorial(-1);
+            Assert.AreEqual(-1, res);
+        }
+
+        [TestMethod]
+        public void CalcularFactorial_DevolverUno_SiCero()
+        {
+            long res = Program.CalcularFactorial(0);
+            Assert.AreEqual(1, res);
+        }
+
+        [TestMethod]
+        public void CalcularFactorial_Cinco_DebeSer120()
+        {
+            long res = Program.CalcularFactorial(5);
+            Assert.AreEqual(120, res);
         }
     }
 }
